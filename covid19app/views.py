6c2311ecc7ea6ed99bee35db21ca0ae7349ed58a
deepu97 <deepanshujayant@gmail.com>
2020-10-19 02:33:54 +0530

    workingcountry

--- covid19app/views.py
+++ covid19app/views.py
@@ -1,22 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.http import JsonResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 import requests
 # Create your views here.
 from .models import Corona
 from .forms import OrderForm, CreateUserForm
 
 
-
+@csrf_exempt
 def home(request):
     #print("Running")
     if request.method == 'POST':
        username = request.POST.get('username')
+       print(username)
        password = request.POST.get('password')
-       user = authenticate(request, username = username, password = password)
+       print(password)
+       user = authenticate(username = username, password = password)
+       print(user)
        if user is not None:
             login(request, user)
             return redirect('/main')
@@ -26,7 +30,7 @@ def home(request):
 
     return render(request, 'Home.html')
 
-        
+     
 
 def logoutUser(request):
     logout(request)
@@ -35,14 +39,17 @@ def logoutUser(request):
     #return render(request, 'login.html')
 
 def register(request):
+    print("Hello")
     if request.user.is_authenticated:
         return redirect('/main')
     else:
         form = CreateUserForm()
         if request.method == 'POST':
             form = CreateUserForm(request.POST)
+            print(form)
             if form.is_valid():
                 form.save()
+                print(form)
             return redirect('/')  
 
         return render(request, 'register.html', {'form':form})
@@ -68,15 +75,25 @@ def covid(request):
 
 @login_required(login_url='/logout')
 def countryData(request):
-    data = True
+    #sdata = True
     result = None
     countryPick  = None
-    while(data):
-        try:
-            result = requests.get('https://api.covid19api.com/summary')
-            json = result.json()
-            countryPick = json['Countries']
-            data = False
-        except:
-            data = True
-    return render(request, 'Country.html', {'countryPick' : countryPick})
+    countrydata = request.GET.get("country") or 'India'
+    print(countrydata)
+
+    result = requests.get('https://api.covid19api.com/summary')
+    json = result.json()
+
+    countryPick = json['Countries']
+    #print(countryPick)
+    for i in countryPick:
+        #print(print(i['Country']))
+        if(i['Country'] == countrydata):
+            print(i)
+            return render(request, 'Country.html', {'countryPick' : i, 'data': countryPick})
+
+            
+        
+        
+    
+
